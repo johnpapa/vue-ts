1b4f7c20b2a755367ef0724972940440e5a11053
John Papa <john@johnpapa.net>
2020-10-05 12:44:57 -0400

    creating the data with a class ctor works.

--- src/products.ts
+++ src/products.ts
@@ -8,22 +8,12 @@ export class Product {
 }
 
 export const products: Product[] = [
-  {
-    id: 10,
-    name: 'Strawberries',
-    description: '16oz package of fresh organic strawberries',
-    quantity: 1,
-  },
-  {
-    id: 20,
-    name: 'Sliced bread',
-    description: 'Loaf of fresh sliced wheat bread',
-    quantity: 1,
-  },
-  {
-    id: 30,
-    name: 'Apples',
-    description: 'Bag of 7 fresh McIntosh apples',
-    quantity: 1,
-  },
+  new Product(
+    10,
+    'Strawberries',
+    '16oz package of fresh organic strawberries',
+    1,
+  ),
+  new Product(20, 'Sliced bread', 'Loaf of fresh sliced wheat bread', 1),
+  new Product(30, 'Apples', 'Bag of 7 fresh McIntosh apples', 1),
 ];
